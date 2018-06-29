6f27388be03db28ba6391a6a3d4d76d19c4e5de4
Chris Jones <cjones@juicebox.com.au>
2018-06-29 09:56:11 +0800

    AWS CodeDeploy config

---
+++ _deploy/scripts/afterInstall.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+DOMAIN_NAME="domain.com"
+REGION="ap-southeast-2"
+ELB_TYPE="alb" # alb, classic, none
+
+###########################################
+DEPLOY_ID=${DEPLOYMENT_ID:2:9}
+DEPLOY_DIRECTORY=$(cd `dirname $0` && pwd)
+
+if [ ! -z "$DOMAIN_NAME" ]; then
+
+    # CREATE WORDPRESS UPLOADS FOLDER IF IT DOESNT EXIST
+    if [ ! -d "/var/www/vhosts/${DOMAIN_NAME}/public_html/content/uploads" ]; then
+        mkdir -p /var/www/vhosts/${DOMAIN_NAME}/public_html/content/uploads
+    fi
+
+    # FIX UPLOADS FOLDER PERMISSIONS
+    if [ -d "/var/www/vhosts/${DOMAIN_NAME}/public_html/content/uploads" ]; then
+        chown -R apache:apache /var/www/vhosts/${DOMAIN_NAME}/public_html/content/uploads
+        find /var/www/vhosts/${DOMAIN_NAME}/public_html/content/uploads -type d -exec chmod 755 {} \;
+        find /var/www/vhosts/${DOMAIN_NAME}/public_html/content/uploads -type f -exec chmod 644 {} \;
+    fi
+
+    # COPY WORDPRESS CONFIGURATION FILES FROM S3 BUCKET
+    /usr/bin/aws s3 sync s3://${DOMAIN_NAME}-assets/ /var/www/vhosts/${DOMAIN_NAME}/public_html/ --quiet --region ap-southeast-2 --exclude '*' --include 'wp-config.php'
+    chmod 644 /var/www/vhosts/${DOMAIN_NAME}/public_html/wp-config.php
+
+    # COPY UPDATED VIRTUAL HOST FILE TO APACHE CONF FOLDER
+    if [ -f /var/www/vhosts/${DOMAIN_NAME}/_deploy/${DOMAIN_NAME}.conf ]; then
+        cp /var/www/vhosts/${DOMAIN_NAME}/_deploy/${DOMAIN_NAME}.conf /etc/httpd/vhosts/${DOMAIN_NAME}.conf
+    fi
+
+    # RECURSIVELY COPY ALL STATIS FILES TO S3 BUCKET
+    /usr/bin/aws s3 sync /var/www/vhosts/${DOMAIN_NAME}/public_html/ s3://${DOMAIN_NAME}-assets/build/${DEPLOY_ID}/ --quiet --region ${REGION} --acl public-read --cache-control max-age=31536000,public --exclude '*' --include '*.css' --include '*.js' --include '*.svg' --include '*.jpg' --include '*.png'  --include '*.eot' --include '*.ttf' --include '*.woff' --include '*.woff2'
+
+    # INSERT CDN VERSION KEY INTO WP-CONFIG.PHP
+    NEW_CDN_VERSION_KEY="%%CDN_VERSION_KEY%%"
+    sed -i "s/${NEW_CDN_VERSION_KEY}/${DEPLOY_ID}/g" /var/www/vhosts/${DOMAIN_NAME}/public_html/wp-config.php
+
+    # SECURE WORDPRESS CONFIGURATION
+    chmod 444 /var/www/vhosts/${DOMAIN_NAME}/public_html/wp-config.php
+
+    # START APACHE
+    systemctl start httpd.service
+
+    # APPLICATION LOAD BALANCER
+    if [ ${ELB_TYPE} == "alb" ]; then
+
+        # REGISTER EC2 INSTANCES WITH ELB TARGET GROUPS
+        . ${DEPLOY_DIRECTORY}/alb/common_functions.sh
+        . ${DEPLOY_DIRECTORY}/alb/register_with_elb.sh
+    fi
+
+    # ELB CLASSIC
+    if [ ${ELB_TYPE} == "classic" ]; then
+
+        # REGISTER EC2 INSTANCES WITH ELB TARGET GROUPS
+        . ${DEPLOY_DIRECTORY}/elb-classic/common_functions.sh
+        . ${DEPLOY_DIRECTORY}/elb-classic/register_with_elb.sh
+    fi
+fi
---
+++ _deploy/scripts/beforeInstall.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+DOMAIN_NAME="domain.com"
+REGION="ap-southeast-2"
+ELB_TYPE="alb" # alb, classic, none
+AWS_CONFIG_BUCKET=""
+
+###########################################
+DEPLOY_ID=${DEPLOYMENT_ID:2:9}
+DEPLOY_DIRECTORY=$(cd `dirname $0` && pwd)
+
+if [ ! -z "$DOMAIN_NAME" ]; then
+
+    # APPLICATION LOAD BALANCER
+    if [ ${ELB_TYPE} == "alb" ]; then
+
+        # UPDATE ALB COMMON_FUNCTIONS.SH
+        rm -rf ${DEPLOY_DIRECTORY}/alb/common_functions.sh
+        /usr/bin/aws s3 cp s3://${AWS_CONFIG_BUCKET}/_deploy/scripts/alb/common_functions.sh ${DEPLOY_DIRECTORY}/alb/common_functions.sh --quiet --region ${REGION}
+        chmod 644 ${DEPLOY_DIRECTORY}/alb/common_functions.sh
+
+        # DREGISTER EC2 INSTANCE FROM ELB target groups
+        . ${DEPLOY_DIRECTORY}/alb/common_functions.sh
+        . ${DEPLOY_DIRECTORY}/alb/deregister_from_elb.sh
+    fi
+
+    # CLASSIC LOAD BALANCER
+    if [ ${ELB_TYPE} == "classic" ]; then
+
+        # UPDATE CLASSIC LOAD BALANCER COMMON_FUNCTIONS.SH
+        rm -rf ${DEPLOY_DIRECTORY}/elb-classic/common_functions.sh
+        /usr/bin/aws s3 cp s3://${AWS_CONFIG_BUCKET}/_deploy/scripts/elb-classic/common_functions.sh ${DEPLOY_DIRECTORY}/elb-classic/common_functions.sh --quiet --region ${REGION}
+        chmod 644 ${DEPLOY_DIRECTORY}/elb-classic/common_functions.sh
+
+        # DREGISTER EC2 INSTANCE FROM ELB
+        . ${DEPLOY_DIRECTORY}/elb-classic/common_functions.sh
+        . ${DEPLOY_DIRECTORY}/elb-classic/deregister_from_elb.sh
+    fi
+
+    # STOP APACHE
+    systemctl stop httpd.service
+
+    # GRAB THE OLD CDN KEY FROM WP-CONFIG.PHP AND DELETE OLD FILES
+    if [ -f /var/www/vhosts/${DOMAIN_NAME}/public_html/wp-config.php ]; then
+        OLD_CDN_VERSION_KEY=`cat /var/www/vhosts/${DOMAIN_NAME}/public_html/wp-config.php | grep CDN_VERSION_KEY | cut -d \' -f 4`
+
+        if [ ! -z "$OLD_CDN_VERSION_KEY" ]; then
+            /usr/bin/aws s3 rm s3://${DOMAIN_NAME}-assets/build/${OLD_CDN_VERSION_KEY}/ --quiet --recursive --region ${REGION}
+        fi
+    fi
+
+    # REMOVE OUR WEBSITE FILES IF THEY ALREADY EXIST
+    if [ -d "/var/www/vhosts/${DOMAIN_NAME}" ]; then
+        rm -rf /var/www/vhosts/${DOMAIN_NAME}/*
+    fi
+
+fi
